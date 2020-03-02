fc08557893b0c5b3c691b1bc1f9326e8b7d82200
TkkPrs <38228254+TkkPrs@users.noreply.github.com>
2020-03-02 21:42:43 +0100

    simple calculator task

---
+++ Simple Calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            InputConverter ic = new InputConverter();
+            CalculatorEngine ce = new CalculatorEngine();
+            double first_number  = ic.convertToDouble(Console.ReadLine());
+            double second_number = ic.convertToDouble(Console.ReadLine());
+            string opertaion = Console.ReadLine();
+            double result = ce.operate(first_number, second_number, opertaion);
+            Console.WriteLine(result);
+            }
+            catch(Exception ex)//
+            {
+                //log exceptions
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
